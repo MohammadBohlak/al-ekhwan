67bebf677f4a0d36b40e003bfeb8bfebcd439c74
MohammadBohlak <mohammad.bohlak5@gmail.com>
2025-08-12 02:59:21 +0300

    ..

--- src/animations/animations.js
+++ src/animations/animations.js
@@ -4,7 +4,7 @@ export const titleAnimation = {
   initial: { scale: 0, y: -50 },
   whileInView: { scale: 1, y: 0 },
   transition: { duration: 1, delay: "0.2" },
-  // viewport: { once: true },
+  viewport: { once: true },
 };
 // export const titleAnimation = {
 //   initial: { clipPath: "polygon(0 0, 100% 0, 100% 0, 0 0)" },
@@ -17,5 +17,5 @@ export const sectionAnimation = {
   initial: { opacity: 0, y: 100 },
   whileInView: { opacity: 1, y: 0 },
   transition: { duration: 0.5, delay: 0.2, ease: "linear" },
-  // viewport: { once: true },
+  viewport: { once: true },
 };
